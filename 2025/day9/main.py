4e94bf5f45b98bf0b4277665fafa8ae2afe74ef5
simssousa15 <sbesousa@gmail.com>
2025-12-09 18:32:22 +0100

    day9 pt.1

---
+++ 2025/day9/main.py
@@ -0,0 +1,44 @@
+import sys
+import math
+
+four_dir = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+diag = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+eight_dir = four_dir + diag
+
+if len(sys.argv) < 3:
+    print("Usage: python main.py <part_nb> <filename>")
+    sys.exit(1)
+
+part = sys.argv[1]
+filename = sys.argv[2]
+
+with open(filename, "r") as file:
+    input = file.read()
+
+def part1(input):
+    # Parse input
+    points = []
+    for line in input.splitlines():
+        strs = line.split(",")
+        points.append((int(strs[0]),int(strs[1])))
+    
+    res = -1
+    for i in range(len(points)):
+        p1 = points[i]
+        for j in range(i, len(points)):
+            p2 = points[j]
+            length = abs(p2[0] - p1[0]) + 1
+            width =  abs(p2[1] - p1[1]) + 1
+            area = length * width
+            res = max(res, area)
+    
+    return res
+
+def part2(input):
+    
+    return "Part 2 result"
+
+if part == "1":
+    print(part1(input))
+elif part == "2":
+    print(part2(input))

7bb32cfe9f234487b1113fb7df33f0dcc7d0c4ec
simssousa15 <sbesousa@gmail.com>
2025-12-11 20:35:59 +0100

    day9 pt.2 [wrong]

--- 2025/day9/main.py
+++ 2025/day9/main.py
@@ -35,8 +35,34 @@ def part1(input):
     return res
 
 def part2(input):
+    # Parse input
+    points = []
+    for line in input.splitlines():
+        strs = line.split(",")
+        points.append((int(strs[0]),int(strs[1])))
     
-    return "Part 2 result"
+    res = -1
+    old_side = -1
+    for i in range(-1, len(points)-1):
+        p1 = points[i]
+        p2 = points[i+1]
+        l = abs(p2[0] - p1[0])
+        w =  abs(p2[1] - p1[1])
+        if(l > w):
+            side = l
+            assert w == 0
+        else:
+            side = w
+            assert l == 0
+        
+        if(old_side != - 1):
+            area = (old_side+1)*(side+1)
+            res = max(area, res)
+            print(p1, p2, res)
+        old_side = side
+
+    
+    return res
 
 if part == "1":
     print(part1(input))

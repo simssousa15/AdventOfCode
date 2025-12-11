bd6129d01700518a02434468783dc9b0ddf5a2cd
simssousa15 <sbesousa@gmail.com>
2025-12-11 21:44:16 +0100

    day10 pt.2 [too slow]

--- 2025/day10/main.py
+++ 2025/day10/main.py
@@ -20,11 +20,7 @@ class State:
         self.ind = indicators
         self.pre = presses
 
-def hash(list_to_hash):
-    chars = ['1' if i else '0' for i in list_to_hash]
-    return "".join(chars)
-
-def min_presses(indicators, buttons):
+def min_presses1(indicators, buttons):
 
     ind = [ i == '.' for i in indicators]
 
@@ -47,9 +43,9 @@ def min_presses(indicators, buttons):
 
             if(all(new_ind)):
                 return new_pre
-            elif(hash(new_ind) not in registry):
+            elif(str(new_ind) not in registry):
                 q.append(State(new_ind, new_pre))
-                registry.add(hash(new_ind))
+                registry.add(str(new_ind))
     assert False
             
 
@@ -78,15 +74,77 @@ def part1(input):
     
     sum = 0
     for i in range(len(ind)):
-        press = min_presses(ind[i], but[i])
+        press = min_presses1(ind[i], but[i])
         print(i, " - ", press)
         sum += press
 
     return sum
 
+def min_presses2(jolt, buttons):
+
+
+    q = []
+    init = State([0 for _ in jolt], 0)
+    q.append(init)
+
+    # very simple to avoid repeated states of indicators
+    # to be implemented later
+    registry = set()
+    while q:
+        # if(len(q)%100==0):
+        #     print(len(q))
+
+        st = q.pop(0)
+        for but in buttons:
+            new_ind = st.ind.copy()
+            new_pre = st.pre + 1
+
+            cont = False
+            for b in but:
+                if(new_ind[b] == jolt[b]):
+                    cont = True
+                    break
+                new_ind[b] += 1
+            if(cont):
+                continue
+
+            if( new_ind == jolt ):
+                return new_pre
+            elif(str(new_ind) not in registry):
+                q.append(State(new_ind, new_pre))
+                registry.add(str(new_ind))
+    assert False
+
 def part2(input):
     
-    return "Part 2 result"
+    lines = input.splitlines()
+    lines_parsed = [l.split(" ") for l in lines]
+
+    ind = []
+    but = []
+    jolt = []
+    for lin in lines_parsed:
+        tmp_but = []
+        for block in lin:
+            if block[0] == "[": # indicator light
+                ind.append(block[1:-1])
+            elif block[0] == "(": # button wiring schematics
+                tmp_but.append([int(i) for i in block[1:-1].split(",")])
+            elif block[0] == "{": # joltage requirements
+                jolt.append([int(i) for i in block[1:-1].split(",")])
+        but.append(tmp_but)
+    
+    print(ind)
+    print(but)
+    print(jolt)
+    
+    sum = 0
+    for i in range(len(ind)):
+        press = min_presses2(jolt[i], but[i])
+        print(i, " - ", press)
+        sum += press
+
+    return sum
 
 if part == "1":
     print(part1(input))
